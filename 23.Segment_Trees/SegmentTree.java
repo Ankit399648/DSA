19851d96d467db4d37d625fcaf36fffa8db6d33e
Ankit399648 <ap806455@gmail.com>
2026-05-29 06:42:56 +0530

    started max min st

--- 23.Segment_Trees/SegmentTree.java
+++ 23.Segment_Trees/SegmentTree.java
@@ -38,6 +38,26 @@ public class SegmentTree {
         }
     }
 
+    public static void updateUtil(int i, int si, int sj, int idx, int diff) {
+        if(idx > sj || idx < si) {
+            return;
+        }    
+
+        tree[i] += diff;
+        if(si != sj) {
+            int mid = (si + sj) / 2;
+            updateUtil(2*i+1, si, mid, idx, diff);
+            updateUtil(2*i+2, mid+1, sj, idx, diff);
+        }
+    }
+
+    public static void update(int[] arr, int idx, int newVal) {
+        int n = arr.length;
+        int diff = newVal - arr[idx];
+        arr[idx] = newVal;
+        updateUtil(0, 0, n-1, idx, diff);
+    }
+
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8};
         int n = arr.length;    
@@ -45,6 +65,8 @@ public class SegmentTree {
         buildST(arr, 0, 0, n-1);
         for(int num: tree) System.out.print(num+" ");
         System.out.println();
+        System.out.println(getSum(arr, 2, 5));
+        update(arr, 2, 2);
         System.out.println(getSum(arr, 2, 5));
     }
 }
