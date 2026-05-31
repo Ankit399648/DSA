263a476d036e891d4af877afec28df165d011c4b
Ankit399648 <ap806455@gmail.com>
2026-05-31 15:51:13 +0530

    Completed segment trees

--- 23.Segment_Trees/Max_MinSegmentTree.java
+++ 23.Segment_Trees/Max_MinSegmentTree.java
@@ -1,3 +1,4 @@
+// this is for the max Segment Tree
 public class Max_MinSegmentTree {
     static int[] tree;
 
@@ -16,6 +17,48 @@ public class Max_MinSegmentTree {
         buildTree(2*i+2, mid+1, sj, arr);
         tree[i] = Math.max(tree[2*i+1], tree[2*i+2]);
     }
+
+    public static int getMax(int[] arr, int qi, int qj) {
+        int n = arr.length;
+        return getMaxUtil(0, 0, n-1, qi, qj);
+    }
+
+    public static int getMaxUtil(int i, int si, int sj, int qi, int qj) {
+        if(si > qj || sj < qi) { // no onverlap
+            return Integer.MIN_VALUE;
+        }
+        else if(si >= qi && sj <= qj) { // complete overlap
+            return tree[i];
+        }
+        else {
+            int mid = (si + sj) / 2;
+            int leftAns = getMaxUtil(2*i+1, si, mid, qi, qj);
+            int rightAns = getMaxUtil(2*i+2, mid+1, sj, qi, qj);
+            return Math.max(leftAns, rightAns);
+        }
+    }
+
+    public static void update(int[] arr, int idx, int newVal) {
+        int n = arr.length;
+        updateUtil(0, 0, n-1, idx, newVal);
+    }
+
+    public static void updateUtil(int i, int si, int sj, int idx, int newVal) {
+        if(idx < si || idx > sj) {
+            return;
+        }
+
+        if(si == sj) {
+            tree[i] = newVal;
+        }
+        
+        if(si != sj) {
+            tree[i] = Math.max(tree[i], newVal);
+            int mid = (si + sj) / 2;
+            updateUtil(2*i+1, si, mid, idx, newVal);
+            updateUtil(2*i+2, mid+1, sj, idx, newVal);
+        }
+    }
     public static void main(String[] args) {
         int[] arr = {6, 8, -1, 2, 17, 1, 3, 2, 4};
         int n = arr.length;
@@ -23,5 +66,12 @@ public class Max_MinSegmentTree {
         buildTree(0, 0, n-1, arr);
 
         for(int a : tree) System.out.print(a+" ");
+        System.out.println();
+        int max = getMax(arr, 2, 5);
+        System.out.println(max);
+
+        update(arr, 2, 20);
+        max = getMax(arr, 2, 5);
+        System.err.println(max);
     }
 }
